1bfd19684ed705d3d72545f19c549eaeef01f2a8
Gowtham Uppuluri <ugowthamsai@gmail.com>
2020-08-31 13:02:10 +0530

    First Commit

---
+++ app1.js
@@ -0,0 +1,28 @@
+document.querySelector(".get-jokes").addEventListener("click", getJokes);
+
+function getJokes(e) {
+  const number = document.querySelector('input[type="number"]').value;
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const resp = JSON.parse(this.responseText);
+      let output = "<ul>";
+
+      if (resp.type === "success") {
+        resp.value.forEach((joke) => {
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output += "<li>Some Thing Went Wrong</li>";
+      }
+      output += "</ul>";
+
+      document.querySelector(".jokes").innerHTML = output;
+    }
+  };
+  xhr.send();
+  e.preventDefault();
+}
